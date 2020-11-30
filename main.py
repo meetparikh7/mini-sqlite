514b916657899557d7d668106c9c9d4e70ae5c16
Meet Parikh <meetparikh7@gmail.com>
2020-12-01 00:06:12 +0530

    Add table parsing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import os
 import sqlparse
 import sys
@@ -5,6 +6,7 @@ import sys
 
 class Tables:
     def __init__(self, dirpath):
+        self.tables = []
         with open(os.path.join(dirpath, "metadata.txt")) as f:
             parsing_table = None
             columns = []
@@ -15,9 +17,8 @@ class Tables:
                     parsed = line[1:-1].strip().split(" ")
                     if parsed[0] == "begin":
                         parsing_table = parsed[1]
-                        print("New table", parsing_table)
                     elif parsed[0] == "end":
-                        print("End table", parsing_table, "columns", columns)
+                        self.tables.append(Table(parsing_table, columns, dirpath))
                         parsing_table = None
                         columns = []
                     else:
@@ -26,6 +27,32 @@ class Tables:
                     columns.append(line)
                 else:
                     raise ValueError("Invalid line", og_line)
+
+    def debug(self):
+        for table in self.tables:
+            table.debug()
+            print("")
+
+
+class Table:
+    def __init__(self, name, columns, dirpath):
+        self.name = name
+        self.columns = columns
+        self.data = []
+        with open(os.path.join(dirpath, f"{name}.csv")) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                self.data.append(row)
+
+    def debug(self):
+        print(f"\nPrinting table {self.name}\n")
+        for column in self.columns:
+            print(f"{column}\t", end="")
+        print("")
+        for row in self.data:
+            for column in row:
+                print(f"{column}\t", end="")
+            print("")
 
 
 class Query:
@@ -37,5 +64,5 @@ class Query:
 
 
 if __name__ == "__main__":
-    Tables(sys.argv[1])
+    Tables(sys.argv[1]).debug()
     Query(sys.argv[2]).debug()
